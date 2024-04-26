9dcbb5f7569eac830efb5e9be9584d36cdefd7be
Alexfuz59 <Alexfuz59@users.noreply.github.com>
2024-04-26 07:37:03 +0000

    Deploying to gh-pages from @ Alexfuz59/training_store_shop_ui@051d45e54ce4971a21ee415dded114f1c6b79eb8 🚀

--- config/links.py
+++
@@ -1,6 +0,0 @@
-class Links:
-    HOST = 'http://16.170.215.221'
-    CATALOG_PAGE = f"{HOST}/home"
-    SHOPPING_CART = f"{HOST}/cart"
-    ORDERS_PAGE = f"{HOST}/orders"
-    PAYMENT_COMPLETED = f"{HOST}:9090/web/stripe/payment/success.html"
